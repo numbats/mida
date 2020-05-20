b5db99362fa7a75b9118c0917203bfc63e22f2bc
Stuart Lee <lee.s@wehi.edu.au>
2020-05-20 12:41:18 +1000

    add zoom groups for meeting

---
+++ project/zoom.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+
+groups <- read_csv(here::here("project", "project-groups.csv"), 
+                   col_names = FALSE) %>% 
+  rename(group = X1) %>% 
+  pivot_longer(cols = -group, 
+               names_to = "room", 
+               values_to = "email_address") %>% 
+  filter(!is.na(email_address)) %>% 
+  mutate(room = as.integer(factor(group))) 
+
+# create zoom rooms
+zoom_col_names <- c("Pre-assign Room Name",
+                    "Email Address")
+
+zoom_rooms <- groups %>%
+  select(room, email_address) %>%
+  mutate(room = paste0("room ", room)) %>%
+  setNames(zoom_col_names)
+
+write_csv(zoom_rooms, here::here("projects/", "zoom-groups.csv"))
